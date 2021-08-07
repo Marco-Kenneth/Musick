22c8afe59bf802f00e90f9d65d9843918ddbca45
Kalva014 <kalva014@ucr.edu>
2021-08-07 11:34:08 -0700

    Updated readme

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,26 @@
+/* The backend starts a server that listens at port 3000 for connections */
 const express = require('express')
-http = require('http')
+const app = express()
+const port = 3000
 
-const hostname = 'localhost';
-const port = 8080;
-const app = express();
- 
-app.use((req, res) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is a test server</h1></body></html>');
- 
-});
-const sample_server = http.createServer(app);
- 
-sample_server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// req stands for request
+// res stands for response
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
