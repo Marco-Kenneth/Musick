56cc9636fd019ea29c9ae943002f0cdce90fcf5f
Kalva014 <kalva014@ucr.edu>
2021-08-08 22:03:33 -0700

    Removed extra React Project and moved frontend code into the folder

--- Frontend/musick/src/App.js
+++ Frontend/musick/src/App.js
@@ -1,63 +1,25 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
 
 function clicked(){
-    alert("New functionality coming soon");
+  alert("New functionality coming soon");
 }
-
-
-
-
-function App() {
-  return (
-      <div
-        class="bg_image"
-      >
-      <div class="center">Musick</div> 
-
-      <div class="cbut"> 
-        <button class="no" onClick={clicked}>
-        Continue
-        </button>
-        </div>
-
-      </div>
-  );
-=======
-import React, { Component } from 'react'
 
 // This is the component where everything loads 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Hello, react!</h1>
+              <h1>Hello, react!</h1>
+              <div class="bg_image">
+                <div class="center">Musick</div> 
+              </div>
+              <div class="cbut"> 
+                <button class="no" onClick={clicked}> Continue</button>
+              </div>
             </div>
         )
     }
->>>>>>> upstream/main
 }
 
 export default App
